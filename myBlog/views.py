ac35c51ced522d5a45af1f335dd12212555a525e
toufique <toufique.jami@gmail.com>
2018-09-10 02:59:55 +0600

    updated, view kora jay

--- myBlog/views.py
+++ myBlog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'myBlog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'myBlog/post_list.html', {'posts': posts})
