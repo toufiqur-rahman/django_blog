1b0f1189d8d60b85cc35e1b8faaeae64b94f81a7
toufique <toufique.jami@gmail.com>
2018-09-11 00:55:43 +0600

    final touch...pore abar edit korbo

--- myBlog/views.py
+++ myBlog/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'myBlog/post_list.html', {'posts': posts})
+    Post.objects.get(pk=pk)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'myBlog/post_detail.html', {'post': post})
